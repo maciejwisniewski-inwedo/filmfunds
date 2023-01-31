d25dc5398cb6e39e429fa5d73cb6231a09f9e2b2
maciejwisniewski-inwedo <maciej.wisniewski@inwedo.com>
2023-01-31 12:45:24 +0000

    Code Synced by DhiWise

---
+++ src/components/Input/index.js
@@ -0,0 +1,77 @@
+import { ErrorMessage } from "../../components/ErrorMessage";
+import React from "react";
+import PropTypes from "prop-types";
+
+const variants = {
+  OutlineGray200: "bg-white_A700 border border-gray_200 border-solid",
+};
+const shapes = { RoundedBorder6: "rounded-radius6" };
+const sizes = { sm: "p-[8px]" };
+
+const Input = React.forwardRef(
+  (
+    {
+      wrapClassName = "",
+      className = "",
+      name,
+      placeholder,
+      type = "text",
+      children,
+      errors = [],
+      label = "",
+      prefix,
+      suffix,
+      shape,
+      variant,
+      size,
+      ...restProps
+    },
+    ref
+  ) => {
+    return (
+      <>
+        <div
+          className={`${wrapClassName} ${shapes[shape] || ""} ${
+            variants[variant] || ""
+          } ${sizes[size] || ""}`}
+        >
+          {!!label && label}
+          {!!prefix && prefix}
+          <input
+            ref={ref}
+            className={`${className} bg-transparent border-0`}
+            type={type}
+            name={name}
+            placeholder={placeholder}
+            {...restProps}
+          />
+          {!!suffix && suffix}
+        </div>
+        {!!errors && <ErrorMessage errors={errors} />}
+      </>
+    );
+  }
+);
+
+Input.propTypes = {
+  wrapClassName: PropTypes.string,
+  className: PropTypes.string,
+  name: PropTypes.string,
+  placeholder: PropTypes.string,
+  type: PropTypes.string,
+  shape: PropTypes.oneOf(["RoundedBorder6"]),
+  variant: PropTypes.oneOf(["OutlineGray200"]),
+  size: PropTypes.oneOf(["sm"]),
+};
+Input.defaultProps = {
+  wrapClassName: "",
+  className: "",
+  name: "",
+  placeholder: "",
+  type: "text",
+  shape: "RoundedBorder6",
+  variant: "OutlineGray200",
+  size: "sm",
+};
+
+export { Input };
